a9f1cd3ab1db241c07fff6bace8be4f7519b7b46
Ogbonna Samuel <samuelogbonna30@gmail.com>
2020-04-03 10:57:03 +0100

    Subject: Data Wrangling for the first Visual
    Body:
    The data wrangling script has been updated with teh code required to
    process data for the first visual. The code added returns data in a
    format that can be used by plotly in the fron end.

--- Data/Wrangling.py
+++ Data/Wrangling.py
@@ -1,7 +1,52 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr  3 07:11:09 2020
+The code in this python script will run on the backend of a data dashboard.
 
-@author: chima
+The script will wrangle the dataset from the CSV file and output a format that 
+can be rendred as visuals by frontend library - in this case plotly.
+
+The data being wrangled contains immigration data for 'Albania', 'Australia', 
+'Austria', 'Belgium', 'Bulgaria','Bosnia and Herzegovina','Canada', 'Cyprus', 
+and the 'Czech Republic' between 1999 and 2016.
+
 """
 
+#Importing the relevant libraried
+import pandas as pd
+import plotly.graph_objs as go
+
+#Reading the data from the CSV file into a dataframe 
+df = pd.read_csv("asylum-seekers-monthly.csv")
+df.rename(columns={'Country / territory of asylum/residence': 'Country'}, inplace = True)
+    
+    
+##########################Data for Visual 1##########################
+    
+#Select the required columns and groupby the Year column
+df_v1 = df[['Year', 'Value']]
+df_v1 = df_v1.groupby('Year').Value.sum()
+df_v1 = df_v1.to_frame(name = 'count').reset_index()
+    
+#Create output dictionary for Plotly
+df_v1.sort_values('Year', ascending=True, inplace=True)
+visual_1 = []
+visual_1.append(
+        go.Scatter(
+                x = df_v1['Year'].values.tolist(),
+                y = df_v1['count'].values.tolist(),
+                mode = 'lines'
+               )
+        )
+layout_v1 = dict(
+        title = 'Changes in Total Number of Global Migrants',
+        xaxis = dict(title = 'Year'),
+        yaxis = dict(title = 'Number of Migrants'))
+    
+    
+##########################Data for Visual 2##########################
+
+
+
+##########################Data for Visual 3##########################
+    
+##########################Data for Visual 4##########################
